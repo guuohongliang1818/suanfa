5e95bd6bb527395c9bb508ae043e10967eecdc0c
guohongliang <guohongliang@eyou.com>
2023-05-08 16:37:35 +0800

    二分算法leftMost

--- src/main/java/com/ghl/BinarySearch.java
+++ src/main/java/com/ghl/BinarySearch.java
@@ -1,5 +1,6 @@
 package com.ghl;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class BinarySearch {
@@ -93,6 +94,64 @@ public class BinarySearch {
         return -1;
     }
 
+    /**
+     * 重复的元素返回最左元素的下标
+     *
+     * @param arr
+     * @param target
+     * @return
+     */
+    public static int binarySearchLeftMost(int[] arr, int target) {
+        /**
+         * [1, 3, 6, 7, 7, 7, 9, 12, 35, 36, 67]
+         */
+        int start = 0;
+        int end = arr.length - 1;
+        //候选位置
+        int candidate = -1;
+        while (start <= end) {
+            int mid = (start + end) >>> 1;
+            if (arr[mid] > target) {
+                end = mid - 1;
+            } else if (arr[mid] < target) {
+                start = mid + 1;
+            } else {
+                candidate = mid;
+                end = mid - 1;
+            }
+        }
+        return candidate;
+    }
+
+    /**
+     * 重复的元素返回最左元素的下标
+     *
+     * @param arr
+     * @param target
+     * @return
+     */
+    public static int binarySearchRightMost(int[] arr, int target) {
+        /**
+         * [1, 3, 6, 7, 7, 7, 9, 12, 35, 36, 67]
+         */
+        int start = 0;
+        int end = arr.length - 1;
+        //候选位置
+        int candidate = -1;
+        while (start <= end) {
+            int mid = (start + end) >>> 1;
+            if (arr[mid] > target) {
+                end = mid - 1;
+            } else if (arr[mid] < target) {
+                start = mid + 1;
+            } else {
+                candidate = mid;
+                start = mid + 1;
+            }
+        }
+        return candidate;
+    }
+
     public static void main(String[] args) {
 
         int[] arr = {1, 3, 6, 8, 9, 12, 35, 36, 67};
@@ -121,7 +180,31 @@ public class BinarySearch {
          * 不把最高位视为符号位的结果是：3221225470
          * 把最高位视为符号位的结果是：-1073741826
          */
-        int ind = Arrays.binarySearch(arr,7);
-        System.out.println(ind);
+        int ind = Arrays.binarySearch(arr, 7);
+        /**
+         * ind = -(插入点+1)
+         * 求：插入点=-ind-1
+         */
+        System.out.println("ind:" + ind);
+        System.out.println("插入点:" + (-ind - 1));
+        /**
+         * 将7插入数组arr中
+         *           i
+         * [1, 3, 6, 8, 9, 12, 35, 36, 67]
+         * [1, 3, 6, 7, 8, 9, 12, 35, 36, 67]
+         */
+        if (ind < 0) {
+            int insert = -ind - 1;//插入点索引
+            //1.数组扩容，同时将原数组插入点之前的数据拷贝到新数组中
+            int[] new_array = new int[arr.length + 1];
+            System.arraycopy(arr, 0, new_array, 0, insert);
+            //2.在新数组中添加新的元素
+            new_array[3] = 7;
+            //3.将原数组插入点之后的数据拷贝到新数组中
+            System.arraycopy(arr, insert, new_array, insert + 1, arr.length - insert);
+            System.out.println(Arrays.toString(new_array));
+
+        }
+
     }
 }
--- src/test/java/com/ghl/BinarySearchTest.java
+++ src/test/java/com/ghl/BinarySearchTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 public class BinarySearchTest {
 
     @Test
@@ -60,6 +62,59 @@ public class BinarySearchTest {
         Assertions.assertEquals(-1,BinarySearch.binarySearchBasicAlternative(arr,5));
         Assertions.assertEquals(-1,BinarySearch.binarySearchBasicAlternative(arr,10));
         Assertions.assertEquals(-1,BinarySearch.binarySearchBasicAlternative(arr,11));
+
+    }
+
+    @Test
+    @DisplayName("binarySearchLeftMost")
+    public void test6(){
+        int[] arr = {1, 3, 8, 8, 9, 12, 12, 36, 67};
+        Assertions.assertEquals(0,BinarySearch.binarySearchLeftMost(arr,1));
+        Assertions.assertEquals(1,BinarySearch.binarySearchLeftMost(arr,3));
+        Assertions.assertEquals(2,BinarySearch.binarySearchLeftMost(arr,8));
+        Assertions.assertEquals(4,BinarySearch.binarySearchLeftMost(arr,9));
+        Assertions.assertEquals(5,BinarySearch.binarySearchLeftMost(arr,12));
+        Assertions.assertEquals(7,BinarySearch.binarySearchLeftMost(arr,36));
+        Assertions.assertEquals(8,BinarySearch.binarySearchLeftMost(arr,67));
+
+        Assertions.assertEquals(-1,BinarySearch.binarySearchLeftMost(arr,-1));
+        Assertions.assertEquals(-1,BinarySearch.binarySearchLeftMost(arr,7));
+        Assertions.assertEquals(-1,BinarySearch.binarySearchLeftMost(arr,100));
+
+    }
+
+    @Test
+    @DisplayName("binarySearchRightMost")
+    public void test7(){
+        int[] arr = {1, 3, 8, 8, 9, 12, 12, 36, 67};
+        Assertions.assertEquals(0,BinarySearch.binarySearchRightMost(arr,1));
+        Assertions.assertEquals(1,BinarySearch.binarySearchRightMost(arr,3));
+        Assertions.assertEquals(3,BinarySearch.binarySearchRightMost(arr,8));
+        Assertions.assertEquals(4,BinarySearch.binarySearchRightMost(arr,9));
+        Assertions.assertEquals(6,BinarySearch.binarySearchRightMost(arr,12));
+        Assertions.assertEquals(7,BinarySearch.binarySearchRightMost(arr,36));
+        Assertions.assertEquals(8,BinarySearch.binarySearchRightMost(arr,67));
+
+        Assertions.assertEquals(-1,BinarySearch.binarySearchRightMost(arr,-1));
+        Assertions.assertEquals(-1,BinarySearch.binarySearchRightMost(arr,7));
+        Assertions.assertEquals(-1,BinarySearch.binarySearchRightMost(arr,100));
+
+    }
+
+    @Test
+    @DisplayName("java二分查找插入点测试")
+    public void test5(){
+        int[] arr = {1, 3, 6, 8, 9, 12, 35, 36, 67};
+        int insert1 = Arrays.binarySearch(arr,4);
+        Assertions.assertEquals(-3,insert1);
+
+
+        int insert2 = Arrays.binarySearch(arr,7);
+        Assertions.assertEquals(-4,insert2);
+
+        int insert3 = Arrays.binarySearch(arr,11);
+        Assertions.assertEquals(-6,insert3);
+
 
     }
 }
