14e358f2f2ba26653580eaf05b2d40cdf862520a
guohongliang <guohongliang@eyou.com>
2023-05-11 10:43:09 +0800

    双向链表总结

--- src/main/java/com/ghl/DoubleLinkedListSentinel.java
+++ src/main/java/com/ghl/DoubleLinkedListSentinel.java
@@ -34,15 +34,24 @@ public class DoubleLinkedListSentinel implements Iterable<Integer> {
 
     @Override
     public Iterator<Integer> iterator() {
+        /**
+         * null <-666 = 1 = 3 = 8 = 5 = 7 = 9 = 999 -> null
+         *         -1   0   1   2   3   4   5    6      7
+         */
         return new Iterator<Integer>() {
+            //从head.next开始,同时不是尾节点
+            Node  p = head.next;
             @Override
             public boolean hasNext() {
-                return false;
+                //return p!=null && p!=tail;
+                return p!=tail;
             }
 
             @Override
             public Integer next() {
-                return null;
+                int value = p.value;
+                p = p.next;
+                return value;
             }
         };
     }
@@ -89,17 +98,15 @@ public class DoubleLinkedListSentinel implements Iterable<Integer> {
     }
 
     public void addLast(int value) {
-        //1.先创建一个新的节点
-        Node node = new Node(null, value, null);
-        //获取头节点和尾节点，尾节点为tail，
-        //头节点为
-        Node head = tail.prev;
-        //处理头节点
-        node.prev = head;
-        head.next = node;
-        //处理尾节点
-        tail.prev = node;
-        node.next = tail;
+        /**
+         * null <-666 = 1 = 3 = 8 = 5 = 7 = 9 = 999 -> null
+         *         -1   0   1   2   3   4   5
+         */
+        //获取 （tail）（tail.prev）
+        //创建新节点
+        Node insert = new Node(tail.prev, value, tail);
+        tail.prev.next = insert;
+        tail.prev = insert;
 
     }
 
@@ -108,6 +115,20 @@ public class DoubleLinkedListSentinel implements Iterable<Integer> {
     }
 
     public void removeLast() {
+        /**
+         * null <-666 = 1 = 3 = 8 = 5 = 7 = 9 = 999 -> null
+         *         -1   0   1   2   3   4   5
+         */
+        //已知哨兵尾节点，所以需要获取 （tail）（tail.prev）(tail.prev.prev)
+        Node removed = tail.prev;
+        if (removed == head) {
+            throw new IllegalArgumentException("参数不合法");
+        }
+        //tail.prev.prev节点
+        Node removed_prev = removed.prev;
+        removed_prev.next = tail;
+        tail.prev = removed_prev;
+
 
     }
 
@@ -143,20 +164,21 @@ public class DoubleLinkedListSentinel implements Iterable<Integer> {
         /**
          * null <-666 = 1 = 3 = 8 = 5 = 7 = 9 = 999 -> null
          *         -1   0   1   2   3   4   5    6      7
-         * 从head.next开始遍历链表，可以查询 index对应的节点信息
+         * 需要找到删除节点，以及删除节点的上一个节点，以及删除节点的下一个节点
          */
-        //1.获取索引下标为index的节点
-        Node node = findNode(index);
+        //获取删除元素的上一个节点
+        Node node = findNode(index - 1);
         if (node == null) {
             throw illegalException(index);
         }
-        //获取当前元素的prev和next
-        Node prev = node.prev;
-        Node next = node.next;
-
-        //修改指向
-        prev.next = next;
-        next.prev = prev;
+        //获取要删除的节点
+        Node removed = node.next;
+        if (removed == tail) {
+            throw illegalException(index);
+        }
+        //改变链表的关系
+        node.next = removed.next;
+        removed.next.prev = node;
 
 
     }
