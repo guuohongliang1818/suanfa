f3fa5e8607fa4b53acca78d5a573385f97bb62ae
guohongliang <guohongliang@eyou.com>
2023-05-12 16:57:06 +0800

    递归二分法

--- src/main/java/com/ghl/algorithm/recursion/E03BinarySearch.java
+++ src/main/java/com/ghl/algorithm/recursion/E03BinarySearch.java
@@ -5,7 +5,7 @@ package com.ghl.algorithm.recursion;
  */
 public class E03BinarySearch {
     /**
-     * [1, 4, 7, 9, 5, 6, 8, 9]
+     * [1, 3, 4, 5, 8, 10, 11, 13]
      */
 
     private static int search(int[] arr, int target) {
@@ -24,7 +24,49 @@ public class E03BinarySearch {
         return -1;
     }
 
-    public static void main(String[] args) {
+    private static int f(int[] arr, int i, int j, int target) {
+        if (i > j) {
+            return -1;
+        }
+        int m = (i + j) >>> 1;
 
+        if (target < arr[m]) {
+            j = m - 1;
+        } else if (arr[m] < target) {
+            i = m + 1;
+        } else {
+            return m;
+        }
+        return f(arr, i, j, target);
+    }
+
+
+    private static int f_left_most(int[] arr, int i, int j, int target) {
+        if (i > j) {
+            return -1;
+        }
+
+        int m = (i + j) >>> 1;
+        int candidate = -1;
+        if (target < arr[m]) {
+            j = m - 1;
+        } else if (arr[m] < target) {
+            i = m + 1;
+        } else {
+            candidate = m;
+            j = m - 1;
+        }
+        return candidate == -1 ? -1 : f(arr, i, j, target);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 4, 5, 8, 10, 11, 13};
+        //System.out.println(search(arr, 4));
+        System.out.println(f(arr, 0, arr.length - 1, -1));
+
+
+        int[] arr1 = {1, 3, 4, 5, 5, 8, 10, 10, 10, 11, 13};
+        //System.out.println(search(arr, 4));
+        System.out.println(f(arr1, 0, arr.length - 1, 5));
     }
 }
