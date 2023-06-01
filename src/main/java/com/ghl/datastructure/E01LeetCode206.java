c836ffd7b6c04a381d49f016cb33b22f3eebd19e
guohongliang <guohongliang@eyou.com>
2023-06-01 16:18:17 +0800

    链表反转方法1已完成

--- src/main/java/com/ghl/datastructure/E01LeetCode206.java
+++ src/main/java/com/ghl/datastructure/E01LeetCode206.java
@@ -2,18 +2,35 @@ package com.ghl.datastructure;
 
 public class E01LeetCode206 {
 
-    public static ListNode reverseList1(ListNode head) {
-        return head;
+    public static ListNode reverseList1(ListNode o1) {
+        ListNode n1 = null;
+        ListNode p = o1;
+        while (p != null) {
+            n1 = new ListNode(p.val, n1);
+            p = p.next;
+        }
+        return n1;
     }
 
-    public static ListNode reverseList2(ListNode head){
-        if(head!=null){
-
+    /**
+     * 构造一个新链表，从旧链表头部移除节点添加到新链表头部，即可完成新链表即是倒序的
+     *
+     * @param o1
+     * @return
+     */
+    public static ListNode reverseList2(ListNode o1) {
+        ListNode p = o1;
+        ListNode n1 = null;
+        while (p != null) {
+            //不用创建新节点，获取旧链表的
+            //将旧链表的next指针指向
+            p.next = n1;
+            //n1 = p.next;
+            p = p.next;
         }
 
 
-
-        return head;
+        return n1;
     }
 
 
@@ -24,7 +41,7 @@ public class E01LeetCode206 {
         ListNode o2 = new ListNode(2, o3);
         ListNode o1 = new ListNode(1, o2);
         System.out.println(o1);
-        ListNode n1 = reverseList1(o1);
+        ListNode n1 = reverseList2(o1);
         System.out.println(n1);
 
 
